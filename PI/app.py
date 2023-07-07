acc200ecdb92f741b01d7f3fbd46e060b6ef0ebe
Danny <121039646@upq.edu.mx>
2023-07-07 17:28:02 -0600

    Creación de Formularios Funcionales, inputs con validaciones y mensajes de cambios realizados.

--- PI/app.py
+++ PI/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for,flash
+from flask import Flask, render_template, request, redirect, url_for,flash, session
 from flask_mysqldb import MySQL
 
 #Estamos declarando el app y le estamos asignando un nombre
@@ -13,14 +13,15 @@ app.config['MYSQL_DB']= "agendabd"
 
 app.secret_key= 'mysecretkey'
 mysql = MySQL(app)
-# Declaramos una ruta 
-# La ruta se compone del nombre y la funcion
-# Declaramos la ruta index http://localhost:5000
+
 
 @app.route('/')
 def index():
     return render_template('Login.html')
 
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -31,47 +32,128 @@ def login():
     CS.execute(consulta, (VEmail, VPassword))
     resultado = CS.fetchone()
     if resultado is not None:
+        # Almacenar el correo electrónico en la sesión
+        session['correo_usuario'] = VEmail
         return render_template('menu.html')
     else:
-        flash('RFC o contraseña incorrectos')
+        flash('Correo o contraseña incorrectos')
         return redirect(url_for('index'))
 
 
 
-@app.route("/perfil")
-def perfil():
-    return render_template("perfil.html")
-
-# @app.route("/perfil", methods=["POST"])
-# def perfil():
-#     if request.method == 'POST':
-#         institucion = request.form['institution']
-#         carrera = request.form['major']
-#         cuatri = request.form['semester']
-        
-#         CS = mysql.connection.cursor()
-#         CS.execute('INSERT INTO usuarios(Institucion, Carrera, Cuatrimestre) VALUES (%s, %s, %s)', (institucion, carrera, cuatri))
-#         mysql.connection.commit()
-
-#         return redirect(url_for('menu'))
-#     return render_template('perfil.html')
-
 
 @app.route('/register')
-def register():
+def registerf():
     return render_template('RegistroUsu.html')
 
-@app.route('/registroUsuarios')
-def registroUsuario():
-    return render_template('menu.html')
+@app.route('/registroUsuario', methods=['POST'])
+def registroUsuariof():
+    if request.method == 'POST':
+        vnombre= request.form['nombre']
+        vemail= request.form['email']
+        vpass= request.form['password']
+        # vcpass= request.form['comfirm-password']
+        
+        CS= mysql.connection.cursor()
+        CS.execute('insert into usuarios(Nombre, Correo_Electronico, Contraseña) values(%s, %s, %s)',(vnombre, vemail, vpass))
+        mysql.connection.commit()
+         # Almacenar los datos en la sesión
+        session['correo_usuario'] = vemail
+    flash('Usuario agregado Correctamente')    
+    return redirect(url_for('index'))
+    
+
+
+@app.route('/perfil')
+def perfilf():
+    return render_template('perfil.html')
+
+
+@app.route('/registroPerfil', methods=['POST'])
+def registroPerfilf():
+    if request.method == 'POST':
+        vinstitucion = request.form['institution']
+        vcarrera = request.form['major']
+        vcuatri = request.form['semester']
+        vfoto = request.form['photo']
+        
+        CS = mysql.connection.cursor()
+        
+        # Obtener el ID del usuario registrado
+        correo_usuario = session['correo_usuario']
+        CS.execute('SELECT ID_Usuario FROM usuarios WHERE Correo_Electronico = %s', (correo_usuario,))
+        id_usuario = CS.fetchone()[0]
+        
+        # Insertar el perfil con el ID de usuario correspondiente
+        CS.execute('INSERT INTO perfil(Institucion, Carrera, Cuatrimestre, Foto, ID_Usuario) VALUES (%s, %s, %s, %s, %s)', (vinstitucion, vcarrera, vcuatri, vfoto, id_usuario))
+        
+        mysql.connection.commit()
+    flash('Perfil agregado correctamente')    
+    return redirect(url_for('menu'))
+    
+    
 
 @app.route("/eventos")
 def creacion_eventos():
     return render_template("eventos.html")
 
+@app.route('/registroEvento', methods=['POST'])
+def eventof():
+    if request.method == 'POST':
+        vtitulo = request.form['title']
+        vdescripcion = request.form['description']
+        vfecha = request.form['date']
+        vhora = request.form['time']
+        vubicacion = request.form['location']
+        
+        
+        CS = mysql.connection.cursor()
+        
+        # Obtener el ID del usuario registrado
+        correo_usuario = session['correo_usuario']
+        CS.execute('SELECT ID_Usuario FROM usuarios WHERE Correo_Electronico = %s', (correo_usuario,))
+        id_usuario = CS.fetchone()[0]
+        
+        # Insertar el perfil con el ID de usuario correspondiente
+        CS.execute('INSERT INTO eventos(Titulo, Descripcion, Fecha, Hora, Ubicacion, ID_Usuario) VALUES (%s, %s, %s, %s, %s,%s)', (vtitulo, vdescripcion, vfecha, vhora, vubicacion, id_usuario))
+        
+        mysql.connection.commit()
+    flash('Evento agregado correctamente')    
+    return redirect(url_for('menu'))
+    
+    
+
 @app.route("/tareas")
 def administracion_tareas():
     return render_template("tareas.html")
+
+
+@app.route('/RegistroTarea', methods=['POST'])
+def registroTareaf():
+    if request.method == 'POST':
+        vtitulo = request.form['titulo']
+        vmateria = request.form['materia']
+        vfechaInicio = request.form['fechaInicio']
+        vfechaFin = request.form['fechaFin']
+        vdescripcion = request.form['descripcion']
+        vprioridad = request.form['prioridad']
+        
+        
+        CS = mysql.connection.cursor()
+        
+        # Obtener el ID del usuario registrado
+        correo_usuario = session['correo_usuario']
+        CS.execute('SELECT ID_Usuario FROM usuarios WHERE Correo_Electronico = %s', (correo_usuario,))
+        id_usuario = CS.fetchone()[0]
+        
+        # Insertar el perfil con el ID de usuario correspondiente
+        CS.execute('INSERT INTO tareas(Titulo_Tarea, Materia, Fecha_Inicio, Fecha_Fin, Descripcion, Prioridad, ID_Usuario) VALUES (%s, %s, %s, %s, %s,%s, %s)', (vtitulo, vmateria, vfechaInicio, vfechaFin, vdescripcion, vprioridad, id_usuario))
+        
+        mysql.connection.commit()
+    flash('Tarea agregada correctamente')    
+    return redirect(url_for('menu'))
+
+    
 
 @app.route("/consultaTareas")
 def consulta_Task():
@@ -88,3 +170,5 @@ def Progreso():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    
